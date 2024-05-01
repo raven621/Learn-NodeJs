3529c0a158e018e22213ffa4575875f32d0f6d21
Chu Ngoc Khanh <chungockhanh2001@gmail.com>
2024-05-01 13:34:54 +0700

    How to create a simple nodejs project

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+// morgan la thu vien in ra cac log khi nguoi dung chay 1 request
+const morgan = require('morgan')
+const {default: helmet} = require('helmet')
+const compression = require('compression')
+
+
+// init middlewares
+app.use(morgan("dev"))
+app. use(helmet())
+app.use(compression())
+// init db
+
+// init routes
+app.get('/', (req, res, next)=> {
+    return res.status(200).json({message: 'Welcome!'})
+})
+
+// handling error
+
+module.exports = app
